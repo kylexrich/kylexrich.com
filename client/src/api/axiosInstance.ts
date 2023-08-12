e09f450ea246a09bd95e18b3bfd9afed9cf1af80
Kyle Rich <kylexrich@gmail.com>
2023-08-12 10:48:44 -0700

    Better Heroku configuration
    Added different setups for dev, staging, and prod

--- client/src/api/axiosInstance.ts
+++ client/src/api/axiosInstance.ts
@@ -1,10 +1,25 @@
 import axios, { AxiosInstance } from "axios";
 
-export const baseUrl: string =
-  process.env.NODE_ENV === "production" ? "https://my.herokuapp.com/api" : "http://localhost:3001/api";
+const env = process.env.REACT_APP_NODE_ENV === "staging" ? "staging" : process.env.NODE_ENV;
+let host: string;
+
+switch (env) {
+  case "development":
+    host = `http://localhost:${3001}/api`;
+    break;
+  case "staging":
+    host = "https://kylexrich-staging-d5a9dbd6a715.herokuapp.com/api";
+    break;
+  case "production":
+    host = "https://kylexrich-402391673bb6.herokuapp.com/api";
+    break;
+  default:
+    host = `http://localhost:${3001}/api`;
+    break;
+}
 
 const api: AxiosInstance = axios.create({
-  baseURL: baseUrl,
+  baseURL: host,
 });
 
 export default api;
